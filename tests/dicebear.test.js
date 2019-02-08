8d2954ee060483d3d4378f0110ab25f50c4d327e
Stanisław Jaros <komkk@protonmail.com>
2019-02-08 16:18:21 +0100

    test fix

--- tests/dicebear.test.js
+++ tests/dicebear.test.js
@@ -1,12 +1,11 @@
 const getAvatar = require('../providers/dicebear');
-const md5 = require('../utils/md5');
 
 describe('dicebear', () => {
   describe('getAvatar()', () => {
     it('returns valid avatar', async () => {
       const data = await getAvatar('test');
 
-      expect(md5(data)).toEqual('18b38c31bf02f5f4e2daef6b2faca131');
+      expect(data.startsWith('<svg')).toBe(true);
     });
   });
 });
